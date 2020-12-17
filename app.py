f57b574ee09c16f6f89c574dc8380ced150855af
katotopark <ozko84@gmail.com>
2020-12-17 22:21:35 +0100

    Correct typo

--- app.py
+++ app.py
@@ -283,4 +283,4 @@ def handle_auth_error(e):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    APP.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
